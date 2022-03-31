b8f45ac92808f31dd7e1e721641f4a35d7640360
Cigmaian Andrei <cigmaian1@gmail.com>
2022-03-31 21:47:47 +0300

    TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -53,3 +53,30 @@ export default class TodoList {
       })
     })
   }
+
+  updateWeekProject() {
+    this.getProject('This week').tasks = []
+
+    this.projects.forEach((project) => {
+      if (project.getName() === 'Today' || project.getName() === 'This week')
+        return
+
+      const weekTasks = project.getTasksThisWeek()
+      weekTasks.forEach((task) => {
+        const taskName = `${task.getName()} (${project.getName()})`
+        this.getProject('This week').addTask(new Task(taskName, task.getDate()))
+      })
+    })
+
+    this.getProject('This week').setTasks(
+      this.getProject('This week')
+        .getTasks()
+        .sort((taskA, taskB) =>
+          compareAsc(
+            toDate(new Date(taskA.getDateFormatted())),
+            toDate(new Date(taskB.getDateFormatted()))
+          )
+        )
+    )
+  }
+}
